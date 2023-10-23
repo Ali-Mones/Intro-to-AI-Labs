2ab872e400cd609ad10f5de35851521e1242444f
Marwan <maroelsays2013@gmail.com>
2023-10-23 23:39:04 +0300

    Added A star Algorithms

--- Lab_1/8 Game/ai_algorithms.py
+++ Lab_1/8 Game/ai_algorithms.py
@@ -1,4 +1,6 @@
 from collections import deque
+from queue import PriorityQueue
+from math import sqrt
 
 parent_map : dict[str,str] = {}
 explored : set[str] = set()
@@ -36,6 +38,8 @@ def get_neighbours (state ) ->list[str] :
         temp[pos-1] = "_"
         neighbours.append("".join(temp))
     return neighbours
+
+
 # ----------------DFS----------------------
 def DFS(init_state) -> dict[str,str]:
     global parent_map , explored , search_depth
@@ -65,7 +69,7 @@ def DFS(init_state) -> dict[str,str]:
     return None
             
         
-  
+# ----------------BFS---------------------- 
 def BFS(init_state) -> dict[str,str]:
     global parent_map , explored , search_depth
     parent_map.clear()
@@ -78,7 +82,7 @@ def BFS(init_state) -> dict[str,str]:
     frontier_set.add(init_state)
     parent_map[init_state] = init_state
     
-    while  frontier :
+    while frontier :
         cur = frontier.popleft()
         explored.add(cur)
         # print(cur)
@@ -92,4 +96,127 @@ def BFS(init_state) -> dict[str,str]:
                 parent_map [neighbour] = cur
                 
     return None
+
+
+# ---------------Utilities--------------------------
+def getpos(pos):
+    row = pos//3
+    col = pos%3
+    return (row,col)
+
+# ----------------A* Manhattan----------------------     
+def M_Heuristic(state:str):
     
+    heuristic:int = 0
+    for i in range(len(state)):
+        if(state[i] != '_'):
+            curr_pos = getpos(i)
+            req_pos =  getpos(state[i]-'0')
+            dist = abs(curr_pos[0]-req_pos[0]) + abs(curr_pos[1]-req_pos[1])
+            heuristic+=dist
+
+    return heuristic
+             
+
+
+def AStarManhattan(init_state) -> dict[str,str]:
+    global parent_map , explored , search_depth
+    parent_map.clear()
+    explored.clear()
+    search_depth = 0
+    Pmap:dict[str,tuple(str,int)] = {}
+    frontier : PriorityQueue[tuple(str,int)] = PriorityQueue()
+    init_heur = M_Heuristic(init_state)
+    frontier.put((init_state,init_heur))
+    # ask question 
+    frontier_set : set[str] = set()
+    frontier_set.add(init_state)
+    Pmap[init_state] = (init_state,init_heur)
+    parent_map[init_state] = init_state
+    
+    while frontier:
+        
+        state = frontier.get()
+        G = state[1] - M_Heuristic(state[0])
+        if(state[0] in explored):
+            continue
+        
+        explored.add(state[0])
+        if state == "_12345678" :
+            return parent_map
+        
+        neighbours = get_neighbours(state)
+        for neighbour in neighbours : 
+            if not (neighbour in frontier_set or neighbour in explored) :
+                heur = M_Heuristic(neighbour)
+                cost = G + 1 + heur
+                frontier.put((neighbour,cost))
+                frontier_set.add(neighbour)
+                parent_map[neighbour] = state[0]
+                Pmap[neighbour] = (state[0],G + 1)
+            elif neighbour in frontier_set:
+                if(G + 1 < Pmap[neighbour][1]):
+                    Pmap[neighbour] = (state[0],G+1)   
+                    parent_map[neighbour] = state[0]      
+    
+    return None 
+
+    
+    
+    
+# ----------------A* Euclidean---------------------- 
+def E_Heuristic(state:str):
+    
+    heuristic:float = 0
+    for i in range(len(state)):
+        if(state[i] != '_'):
+            curr_pos = getpos(i)
+            req_pos =  getpos(state[i]-'0')
+            dist = sqrt(pow(curr_pos[0]-req_pos[0],2) + pow(curr_pos[1]-req_pos[1],2))
+            heuristic+=dist
+    
+    return heuristic
+            
+            
+            
+def AStarEuclidean(init_state) -> dict[str,str]:
+    global parent_map , explored , search_depth
+    parent_map.clear()
+    explored.clear()
+    search_depth = 0
+    Pmap:dict[str,tuple(str,float)] = {}
+    frontier : PriorityQueue[tuple(str,float)] = PriorityQueue()
+    init_heur = E_Heuristic(init_state)
+    frontier.put((init_state,init_heur))
+    # ask question 
+    frontier_set : set[str] = set()
+    frontier_set.add(init_state)
+    Pmap[init_state] = (init_state,init_heur)
+    parent_map[init_state] = init_state
+    
+    while frontier:
+        
+        state = frontier.get()
+        G = state[1] - E_Heuristic(state[0])
+        if(state[0] in explored):
+            continue
+        
+        explored.add(state[0])
+        if state == "_12345678" :
+            return parent_map
+        
+        neighbours = get_neighbours(state)
+        for neighbour in neighbours : 
+            if not (neighbour in frontier_set or neighbour in explored) :
+                heur = E_Heuristic(neighbour)
+                cost = G + 1 + heur
+                frontier.put((neighbour,cost))
+                frontier_set.add(neighbour)
+                parent_map[neighbour] = state[0]
+                Pmap[neighbour] = (state[0],G + 1) 
+            elif neighbour in frontier_set:
+                if(G + 1 < Pmap[neighbour][1]):
+                    Pmap[neighbour] = (state[0],G+1)  
+                    parent_map[neighbour] = state[0]      
+    
+    return None 
