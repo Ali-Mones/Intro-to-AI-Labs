a80399eccba0accc8e349c5c77d4d41ec2537713
Ali Mo'nes <AliIsHomie@outlook.com>
2023-11-28 15:59:49 +0200

    Added more params to test functions

--- Lab_2/tests/time_tests.py
+++ Lab_2/tests/time_tests.py
@@ -5,7 +5,7 @@ from minimax import *
 import time
 
 
-def time_nodes_test(tests: list[list[list[int]]], count: int, depth: int) -> tuple[float, float, float, float]:
+def time_nodes_test(tests: list[list[list[int]]], count: int, depth: int, with_no_pruning: bool) -> tuple[float, float, float, float]:
     t_total = 0
     t_ab_total = 0
     nodes_total = 0
@@ -14,11 +14,12 @@ def time_nodes_test(tests: list[list[list[int]]], count: int, depth: int) -> tup
         board = tests[i]
         col_index = get_col_index(board)
 
-        t = time.time_ns()
-        _, _, nodes = minimax(board, depth, True, col_index)
-        t = time.time_ns() - t
-        t_total += t
-        nodes_total += nodes
+        if with_no_pruning:
+            t = time.time_ns()
+            _, _, nodes = minimax(board, depth, True, col_index)
+            t = time.time_ns() - t
+            t_total += t
+            nodes_total += nodes
 
         t = time.time_ns()
         _, _, nodes = minimax_alphabeta(board, depth, True, col_index)
@@ -29,19 +30,21 @@ def time_nodes_test(tests: list[list[list[int]]], count: int, depth: int) -> tup
     return (t_total / count / 1e6, t_ab_total / count / 1e6, nodes_total / count, nodes_ab_total / count)
 
 
-def time_nodes_diff_depths_test(tests: list[list[list[int]]], count: int, max_depth: int) -> dict[int, tuple[float, float, float, float]]:
+def time_nodes_diff_depths_test(tests: list[list[list[int]]], count: int, min_depth: int, max_depth: int, with_no_pruning: bool) -> dict[int, tuple[float, float, float, float]]:
     times: dict[int, tuple[float, float, float, float]] = {}
 
-    for i in range(1, max_depth + 1):
-        times[i] = time_nodes_test(tests, count, i)
+    for i in range(min_depth, max_depth + 1):
+        times[i] = time_nodes_test(tests, count, i, with_no_pruning)
 
     return times
 
 def main():
     tests = read_tests()
     count = 100
+    min_depth = 1
     max_depth = 8
-    print(time_nodes_diff_depths_test(tests, count, max_depth))
+    with_no_pruning = True
+    print(time_nodes_diff_depths_test(tests, count, min_depth, max_depth, with_no_pruning))
 
 
 if __name__ == "__main__":
